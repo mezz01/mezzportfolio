6fa287e042b45f26df4486da85b942795a14de7d
Amine <mezzatamine2002@gmail.com>
2023-10-27 17:30:00 +0100

    create components and layout

--- components/header.js
+++ components/header.js
@@ -1,5 +1,16 @@
-
+import Link from "next/link";
 
 export default function Header(params) {
-    
+    return(
+        <div>
+            <ul>
+                <Link>Amine Mezzat</Link>
+            </ul>
+            <ul>
+                <Link>Contact</Link>
+                <Link>Work</Link>
+                <Link>About</Link>
+            </ul>
+        </div>
+    )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Header from '../../components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
+      <Header></Header>
       
     </main>
   )
