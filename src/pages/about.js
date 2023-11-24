38f6c699a3eecd809d0b8339c6f3acb305ce122d
Amine <mezzatamine2002@gmail.com>
2023-11-24 17:33:45 +0100

    adding image files

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import Header from "../../components/header";
-
+import Image from "next/image";
 
 export default function about() {
     return(
@@ -8,7 +8,7 @@ export default function about() {
             <h1> A web worker</h1>
             <p> By having a clear conversation about the ultimate outcome and your desire, I can help you get exactly what you're looking for.</p>
             <p></p>
-            
+            <Image src={'/public/favicon.ico'} ></Image>
         </main>
     )    
 }
